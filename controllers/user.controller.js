5a99380e4df63372031973898cd1d94ae72218d4
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-04-07 23:21:06 +0530

    add findOneUser route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,3 +12,16 @@ exports.handleLoginSignup = (req, res) => {
     res.json({ success: false })
   }
 }
+
+exports.findOneUser = (req, res) => {
+  if (req.user) {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    console.log(req.user)
+    res.json({ success: true, userName: req.user.user_name })
+  } else {
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'application/json')
+    res.json({ success: false })
+  }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,21 @@
-const { passport } = require('../authenticate')
+const { passport, verifyUser } = require('../authenticate')
 
 module.exports = app => {
-	const user = require('../controllers/user.controller')
+  const user = require('../controllers/user.controller')
 
-	app.post('/user/login', passport.authenticate('local-login'), user.handleLoginSignup)
+  app.post(
+    '/user/login',
+    passport.authenticate('local-login'),
+    user.handleLoginSignup
+  )
 
-	app.post('/user/signup', passport.authenticate('local-signup'), user.handleLoginSignup)
+  app.post(
+    '/user/signup',
+    passport.authenticate('local-signup'),
+    user.handleLoginSignup
+  )
+
+  app.get('/user/verifyJWT', verifyUser, user.handleLoginSignup)
+
+	app.get('/user', verifyUser, user.findOneUser)
 }
